045ccb3582f74d5ac6bd3cfcd483ea2e82205920
eshantchandak23 <156075287+eshantchandak23@users.noreply.github.com>
2024-01-09 15:55:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title("First Streamlit App")
-streamlit.text("This the first application")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 🐔 Hard-Boiled Free-Range Egg')
+
