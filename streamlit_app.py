b32c117ef57bb462e6a42c37f4f50a269aa52110
eshantchandak23 <156075287+eshantchandak23@users.noreply.github.com>
2024-01-09 17:33:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # create a pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table format
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
